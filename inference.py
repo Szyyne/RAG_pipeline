90041a8023b93c43e4212316435ebd347bb869dd
Shaik Waheed <90918884+Szyyne@users.noreply.github.com>
2026-04-07 15:17:02 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -46,7 +46,7 @@ from models import RAGAction
 API_BASE_URL = os.getenv("API_BASE_URL", "https://router.huggingface.co/v1")
 MODEL_NAME   = os.getenv("MODEL_NAME",   "Qwen/Qwen2.5-72B-Instruct")
 HF_TOKEN     = os.getenv("HF_TOKEN")
-ENV_URL      = os.getenv("ENV_URL",      "http://localhost:7860")
+ENV_URL      = os.getenv("ENV_URL")
 BENCHMARK    = "rag_pipeline_doctor"
 
 MAX_STEPS               = 5
